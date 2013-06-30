1aa9725cb4378514dc9c6464d658f7bb64135860
Renato Gama <renatoargh@gmail.com>
2013-06-30 02:15:38 -0300

    fixed some bugs, feature of returning object containing all registered routes and meta data (if any)

--- example/app.js
+++ example/app.js
@@ -1,4 +1,4 @@
-//run before testing: npm install express portfinder
+//run before testing: npm install express portfinder jade
 
 var
 	express = require("express"),
@@ -6,15 +6,19 @@ var
 	routeRegistrar = require("../routeRegistrar");
 	
 var app = express();
+app.set('view engine', 'jade');
 
-routeRegistrar(__dirname, app, {
-	foo: "bar",
-	importantThing: true
+//route-registrar return an object containing meta info about your routes
+var meta = routeRegistrar(__dirname + "/controllers", app, {
+	foo: "bar"
 });
 
+//you can still register routes everywhere
 app.get("/stillworks", function(req, res, next){
 	res.send("Yes!");
-});
+}, {extra: "This route sends 'Yes!' to the browser"});
+
+console.log(meta);
 
 portfinder.getPort(function(err, port){
 	if(err) throw new Error("Error finding available port");
--- routeRegistrar.js
+++ routeRegistrar.js
@@ -8,17 +8,28 @@ var
 
 module.exports = function(lookUpPath, app, options){
 	methods.push("all");
+	methods.push("del");
+	
+	var routes = [];
 	methods.forEach(function(method){
 		app[method] = _.wrap(app[method], function(fn){
 			var args = oUtils.argsToArray(arguments, 1);
 			
+			var meta = { method: method, route: args[0]};
+			if(args.length > 2 && oUtils.isObject(args[args.length - 1])){
+				meta.meta = args[args.length - 1];
+				aUtils.removeLast(args);
+			} 
+			
+			routes.push(meta);
+			
 			var before = arguments.callee.caller.caller.before;
 			if(before) aUtils.insertAt(args, 1, before);
-
+			
 			var after = arguments.callee.caller.caller.after;
 			if(after) aUtils.insertAt(args, args.length - 1, after);
-			
-			fn.apply(app, args);
+
+			return fn.apply(app, args);
 		});
 	});
 	
@@ -34,4 +45,6 @@ module.exports = function(lookUpPath, app, options){
 			else throw new Error("Controllers must have a 'module.exports.init' function");
 		}
 	});
+	
+	return routes;
 };
