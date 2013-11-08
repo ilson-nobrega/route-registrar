07dc8da8765f6efb64f26359833ab5fb7e4804bb
Renato Gama <renatoargh@gmail.com>
2013-11-08 10:44:42 -0200

    removed before and after methods so it can be used by strict apps

--- routeRegistrar.js
+++ routeRegistrar.js
@@ -7,16 +7,16 @@ var
 	_ = require("underscore");
 
 function pathRegExp(path, keys, sensitive, strict) {
-	//this code comes from express: 
+	//this code comes from express:
 	//https://github.com/visionmedia/express/blob/master/lib/utils.js#L293-L313
-	
+
 	if (toString.call(path) == '[object RegExp]') return path;
 	if (Array.isArray(path)) path = '(' + path.join('|') + ')';
 	path = path
 		.concat(strict ? '' : '/?')
 		.replace(/\/\(/g, '(?:/')
 		.replace(/(\/)?(\.)?:(\w+)(?:(\(.*?\)))?(\?)?(\*)?/g, function(_, slash, format, key, capture, optional, star){
-      
+
 			keys.push({ name: key, optional: !! optional });
 			slash = slash || '';
 			return ''
@@ -29,7 +29,7 @@ function pathRegExp(path, keys, sensitive, strict) {
     })
     	.replace(/([\/.])/g, '\\$1')
     	.replace(/\*/g, '(.*)');
-  
+
 	return new RegExp('^' + path + '$', sensitive ? '' : 'i');
 }
 
@@ -40,7 +40,7 @@ module.exports.register = function(debug){
 	unregisteredRoutes.sort(function(a, b){
 		a = a.args[0].split("/");
 		b = b.args[0].split("/");
-		
+
 		if(a.length !== b.length){
 			return a.length - b.length;
 		}else{
@@ -48,77 +48,77 @@ module.exports.register = function(debug){
 				if(a[i].indexOf(":") === 0) return 1;
 				if(b[i].indexOf(":") === 0) return -1;
 			}
-			
-			return 0; 
+
+			return 0;
 		}
 	});
-	
+
 	unregisteredRoutes.forEach(function(route){
 		if(debug)
 			console.log(route.args[0]);
-		
+
 		route.fn.apply(appBkp, route.args);
 	});
 };
 
 module.exports.find = function(lookUpPath, app, options){
 	appBkp = app;
-	
+
 	methods.push("all");
 	methods.push("del");
-	
+
 	var routes = [];
 	methods.forEach(function(method){
 		app[method] = _.wrap(app[method], function(fn){
 			var args = oUtils.argsToArray(arguments, 1);
-			
+
 			if(args.length === 1 && method === "get")
 				return fn.apply(app, args);
 			else{
 				var params = [];
-				var meta = { 
+				var meta = {
 						method: (method === "del" ? "delete" : method),
 						regexp: pathRegExp(args[0], params, false, false),
 						params: params,
-						path: args[0], 
+						path: args[0],
 						meta: {}
 				};
-				
+
 				if(args.length > 2 && oUtils.isObject(args[args.length - 1])){
 					meta.meta = args[args.length - 1];
 					aUtils.removeLast(args);
-				} 
-				
+				}
+
 				routes.push(meta);
-				
-				var before = arguments.callee.caller.caller.before;
-				if(before) aUtils.insertAt(args, 1, before);
-				
-				var after = arguments.callee.caller.caller.after;
-				if(after) aUtils.insertAt(args, args.length - 1, after);
-				
+
+//				var before = arguments.callee.caller.caller.before;
+//				if(before) aUtils.insertAt(args, 1, before);
+//
+//				var after = arguments.callee.caller.caller.after;
+//				if(after) aUtils.insertAt(args, args.length - 1, after);
+
 				unregisteredRoutes.push({fn: fn, args: args});
 			}
 		});
 	});
-	
+
 	fs.readdirSync(lookUpPath).forEach(function(controllerName){
 		if(controllerName.indexOf("Controller.js") !== -1){
 			var controller = require(path.join(lookUpPath, controllerName));
 			if(controller.init){
-				if(controller.before) controller.init.before = controller.before; 
+				if(controller.before) controller.init.before = controller.before;
 				if(controller.after) controller.init.after = controller.after;
-				
+
 				controller.init(app, options);
 			}
 			else throw new Error("Controllers must have a 'module.exports.init' function");
 		}
 	});
-	
+
 	routes.sort(function(a, b){
 		a = a.path.split("/");
 		b = b.path.split("/");
-		
+
 		if(a.length !== b.length){
 			return a.length - b.length;
 		}else{
@@ -126,10 +126,10 @@ module.exports.find = function(lookUpPath, app, options){
 				if(a[i].indexOf(":") === 0) return 1;
 				if(b[i].indexOf(":") === 0) return -1;
 			}
-			
-			return 0; 
+
+			return 0;
 		}
 	});
-	
+
 	return routes;
 };
